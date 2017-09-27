49c2bfa067057a14b2ccd890c3fd96094d7b6304
ofset <yoshiki.ito.sys@gmail.com>
2017-09-27 15:32:52 +0900

    変更なう

--- app/src/main/java/to/msn/wings/list/MainActivity.java
+++ app/src/main/java/to/msn/wings/list/MainActivity.java
@@ -3,6 +3,7 @@ package to.msn.wings.list;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// リスト
 public class MainActivity extends AppCompatActivity {
 
     @Override
